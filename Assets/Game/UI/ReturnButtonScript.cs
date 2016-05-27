bd106a41b0a9a81470d7d56fa971a0d72530ca76
gxicxigouxa <gxicxi@gmail.com>
2016-05-27 22:44:27 +0900

    bug fix, scene change

---
+++ Assets/Game/UI/ReturnButtonScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ReturnButtonScript : MonoBehaviour {
+
+  // Use this for initialization
+  void Start() {
+
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+
+  public void MoveScene() {
+    //0번 Scene(Main Game) 호출.
+    SceneManager.LoadScene(0);
+
+  }
+}
---
+++ Assets/Game/UI/ShopButtonScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System;
+
+public class ShopButtonScript : MonoBehaviour {
+
+  // Use this for initialization
+  void Start() {
+  }
+
+  // Update is called once per frame
+  void Update() {
+
+  }
+
+  public void MoveScene() {
+    //1번 Scene(shop) 호출.
+    SceneManager.LoadScene(1);
+  }
+}
