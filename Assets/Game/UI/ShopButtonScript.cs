0dfd84a9b05139212573f005a29934ee60b616c8
kbbn2001 <wkddns1@naver.com>
2016-05-28 17:36:09 +0900

    fix the shop
    click button, don`t move scene

--- Assets/Game/UI/ShopButtonScript.cs
+++ Assets/Game/UI/ShopButtonScript.cs
@@ -4,7 +4,8 @@ using UnityEngine.SceneManagement;
 using System;
 
 public class ShopButtonScript : MonoBehaviour {
-
+    public GameObject buttonUI;
+    public GameObject shopCanvas;
   // Use this for initialization
   void Start() {
   }
@@ -15,7 +16,10 @@ public class ShopButtonScript : MonoBehaviour {
   }
 
   public void MoveScene() {
-    //1번 Scene(shop) 호출.
-    SceneManager.LoadScene(1);
+        //1번 Scene(shop) 호출.
+        //SceneManager.LoadScene(1);
+        buttonUI.SetActive(!buttonUI.activeSelf);
+        shopCanvas.SetActive(!shopCanvas.activeSelf);
+
   }
 }
