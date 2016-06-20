9a63885f5f1e24bf3e2e794ea2eab220c92bf716
gxicxigouxa <gxicxi@gmail.com>
2016-06-20 14:55:21 +0900

    Add object spin

---
+++ Assets/Game/Game Object/ChangeColorScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeColorScript : MonoBehaviour {
+  private Material object_material_;
+	// Use this for initialization
+	void Start () {
+    object_material_ = GetComponent<Renderer>().material;
+    gameObject.GetComponent<Renderer>().material.SetColor("_EmissionColor", Color.yellow);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+  }
+}
---
+++ Assets/Game/Game Object/SpinObjectScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinObjectScript : MonoBehaviour {
+  public static float kSpinSpeed = 0.5F;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    transform.Rotate(0.0F, kSpinSpeed, 0.0F);
+	}
+}
